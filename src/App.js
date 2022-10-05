5e723ea3ea874ddddc29822f34597fe8ca05298c
Alperen Guntekin <41083848+AlperenGuntekin@users.noreply.github.com>
2022-10-05 09:44:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,9 +36,6 @@ function App() {
                 <Link 
                 className="nav-link text-white"
                 to="/settings">Ayarlar</Link>
-                <Link 
-                className="nav-link text-white"
-                to="/settings">Ayarlar</Link>
               </div>
             </div>
           </div>
